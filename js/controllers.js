406c27a9c5540bb8e15bbc2bbd2ffc8c04bc01ef
Edmund Mai <edmundmai@gmail.com>
2014-04-19 06:47:17 +0800

    updated

--- js/controllers.js
+++ js/controllers.js
@@ -1,26 +1,28 @@
-	toDoListApp.controller("ChoresController", function($scope) {
-		$scope.chores = [{
-			name: "laundry",
-			hours: 3
-		},
-		{
-			name: "dishes",
-			hours: 0.5
+toDoListApp.controller("ChoresController", function($scope) {
+	$scope.chores = [{
+		name: "laundry",
+		hours: 3
+	},
+	{
+		name: "dishes",
+		hours: 0.5
+	}
+	];
+	$scope.submit = function() {
+		if ($scope.chore.name) {
+			$scope.chores.push({name: $scope.chore.name, hours: 2});
+			$scope.chore.name = '';
+		} else {
+			alert("none");
 		}
-		];
-		$scope.submit = function() {
-			if ($scope.chore.name) {
-				$scope.chores.push({name: $scope.chore.name, hours: 2});
-				$scope.chore.name = '';
-			} else {
-				alert("none");
-			}
-		};
-		$scope.removeChore = function(unwanted_chore) {
-			var index = $scope.chores.indexOf(unwanted_chore);
-			alert($scope.chores.length);
-			$scope.chores.splice(index, 1);
-			alert($scope.chores.length);
-			// $scope.$apply();
-		};
-	});
+	};
+	$scope.removeChore = function(unwanted_chore) {
+		var index = $scope.chores.indexOf(unwanted_chore);
+		console.log($scope.chores.length);
+		alert($scope.chores.length);
+		$scope.chores.splice(index, 1);
+		console.log($scope.chores.length);
+		console.log($scope.chores);
+		// $scope.$apply();
+	};
+});
